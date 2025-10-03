6baf82b1fdde44ec1490859c182480de89f6d64e
ferssae <ferrnandessarah@gmail.com>
2025-10-02 22:26:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => {
     document.body.classList.remove("show-mobile-menu");
 });
+  
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
